2414082bca617bb92a1e02c826fed5894e1e619a
japhet125 <124853679+japhet125@users.noreply.github.com>
2023-12-19 23:43:00 -0500

    PREWORK CODEPATH

---
+++ CodepathPre/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CodepathPre
+//
+//  Created by Guibril Ramde on 12/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBAction func ChangeBackgroundColor(_ sender: UIButton) {
+        let randomColor = changeColor()
+        view.backgroundColor = randomColor
+    }
+    
+    func changeColor() -> UIColor{
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 0.5)
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
